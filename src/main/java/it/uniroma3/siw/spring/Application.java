ffe53b5f0011f33029e1e2047a7f76aac45bf431
unknown <paolo.moser2199@gmail.com>
2021-06-10 16:02:34 +0200

    creato package

--- src/main/java/it/uniroma3/siw/spring/Application.java
+++ src/main/java/it/uniroma3/siw/spring/Application.java
@@ -1,13 +1,13 @@
-package it.uniroma3.siw;
+package it.uniroma3.siw.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SiwRistoranteApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SiwRistoranteApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
