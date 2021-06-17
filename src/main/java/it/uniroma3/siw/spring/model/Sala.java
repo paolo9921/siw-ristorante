005584a6dc301a16aaee05e78845c7ef76ecbcdd
unknown <paolo.moser2199@gmail.com>
2021-06-17 12:18:01 +0200

    Update Sala.java

--- src/main/java/it/uniroma3/siw/spring/model/Sala.java
+++ src/main/java/it/uniroma3/siw/spring/model/Sala.java
@@ -1,5 +1,57 @@
 package it.uniroma3.siw.spring.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Sala {
 
+	//public static final Integer POSTI_MAX = 100;
+	public static final Long ID_SALA = Long.valueOf(1);
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	//@Column
+	//private int postiLiberi = POSTI_MAX.intValue();
+	
+	@OneToMany
+	private List<Tavolo> tavoli;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/*public int getPostiLiberi() {
+		return postiLiberi;
+	}
+
+	public void setPostiLiberi(int postiLiberi) {
+		this.postiLiberi = postiLiberi;
+	}
+
+	public static int getPostiMax() {
+		return POSTI_MAX;
+	}*/
+
+	public List<Tavolo> getTavoli() {
+		return tavoli;
+	}
+
+	public void setTavoli(List<Tavolo> tavoli) {
+		this.tavoli = tavoli;
+	}
+	
+	
 }
