3664d9d9722ecc46313b09109806d5fa6f9f5554
ale9912 <72856621+ale9912@users.noreply.github.com>
2021-06-18 18:03:54 +0200

    creati controller

---
+++ src/main/java/it/uniroma3/siw/spring/controller/MainController.java
@@ -0,0 +1,14 @@
+package it.uniroma3.siw.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@RequestMapping(value="/contatti",method = RequestMethod.GET)
+	public String getContatti(Model model) {
+	return "contatti.html";
+	}
+}
